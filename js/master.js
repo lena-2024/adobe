14eee8fffd153ffeb9eece3ecd8929788406a45a
lena-2024 <rainning2022@gmail.com>
2024-09-26 21:43:57 +0800

    完成專案

--- js/master.js
+++ js/master.js
@@ -1,38 +1,94 @@
-// console.log($(window).width() <= 480) ;
+// console.log($(window).width() <= 480)
 
 // let isMobile;
 
-// if ($(window).width() <= 480) {
+// if($(window).width() <= 480) {
 //     isMobile = true;
 // }else {
 //     isMobile = false;
 // }
-// console.log(isMobile);
-
-const isMobile = $(window).width() <= 480
-const $grid = $('#Grid')
 
 // console.log(isMobile);
 
-function setGrid(){
-    if (isMobile === true) {
-        $grid.addClass('grid-2');
-    } else {
-        $grid.addClass('grid-3');
-    } 
+const isMobile = $(window).width() <= 480;
+const $grid = $('#Grid');
+const $nav = $('#Nav');
+
+// console.log(isMobile);
+
+// console.log(1 == '1'); // true
+// console.log(1 === '1'); // false
+
+function setGrid() {
+  if (isMobile === true) {
+    $grid.addClass('grid-2');
+  } else {
+    $grid.addClass('grid-3');
+  }
 }
 
-function setInit(){
-    setGrid();
+
+function setScroll() {
+
+  if (isMobile === true) {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() === 0) {
+        $nav.removeClass('nav-active');
+      } else {
+        $nav.addClass('nav-active');
+      }
+    });
+
+  }
+
 }
+
+function setInit() {
+  setGrid();
+}
+
+function setEvent() {
+  setScroll();
+}
+
+const $anchors = $('[data-fancybox]');
+$anchors.fancybox({
+  protect: true,
+  loop: true,
+  // animationDuration: 1000,
+  transitionDuration: 1000,
+  transitionEffect: "rotate",
+  keyboard: false,
+  fullScreen: {
+    autoStart: true
+  },
+  infobar: false,
+  toolbar: true,
+  smallBtn: true,
+  // arrows: false,
+  slideShow: {
+    autoStart: false,
+    speed: 1000,
+  },
+  thumbs: true,
+  buttons: [
+    'share',
+    'zoom',
+    'slideShow'
+  ],
+  media: {
+    youtube: {
+      params: {
+        autoplay: false,
+      }
+    }
+  }
+});
+
+
+
+
 
 setInit();
-
-
-
-
-
-
-
-
+setEvent();
 
